f41ae05425cfc47a2dad76d32b75cdf71c96e123
Cristianp24 <cristian-.p@hotmail.com>
2022-10-21 19:08:21 -0300

    acomodando pc

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Viva henry";
+const nuevaString = "Hola henry";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 15;
